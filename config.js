77968bb3145bd1c44dc5d79ee6c0af5c0afeb2b2
angrycoderz <angrycoderz01@gmail.com>
2023-02-10 00:24:45 +0500

    front

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
-import nextConfig from "next.config";
+// import nextConfig from "next.config";
 
-export const APP_NAME = nextConfig.APP_NAME;
-export const API = nextConfig.API;
-export const STRIPE_KEY = nextConfig.STRIPE_PUBLISHABLE_KEY
+// export const APP_NAME = nextConfig.APP_NAME;
+// export const API = nextConfig.API;
+// export const STRIPE_KEY = nextConfig.STRIPE_PUBLISHABLE_KEY
+
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME;
+export const API = process.env.NEXT_PUBLIC_API;
+export const STRIPE_KEY = process.env.NEXT_PUBLIC_STRIPE_KEY
